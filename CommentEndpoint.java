29349e0cb108cbd66f573d4d3de52be7626e8a7f
denys.burmistrov <dburm@softserveinc.com>
2023-07-24 13:24:58 +0300

    UC#12 Create documentation

--- CommentEndpoint.java
+++ CommentEndpoint.java
@@ -9,21 +9,48 @@ import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.comment.CommentDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
 
+/**
+* CommentEndpoint is a class for sending REST requests to "/comments" endpoint
+*
+* @author      Vasyl Boliuk
+* @version     %I%, %G%
+*/
 public class CommentEndpoint extends AbstractWebEndpoint {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String COMMENTS_END = "/comments";
     private static final String COMMENTS_RESOURCE_END = "/comments/{commentID}";
 
+    /**
+    * Creates an instance of CommentEndpoint
+    *
+    * @param specification request specification for HTTP request
+    */
     public CommentEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+    * Sends POST request to "/comments" endpoint,
+    * creates a new comment
+    * then extracts response body as CommentDto class instance
+    *
+    * @param commentDto body of a request containing information about comment
+    * @return response body extracted into CommentDto class instance
+    */
     public CommentDto create(CommentDto commentDto) {
         return create(commentDto, HttpStatus.CREATED)
             .extract().as(CommentDto.class);
     }
 
+    /**
+    * Sends POST request to "/comments" endpoint,
+    * creates a new comment
+    *
+    * @param commentDto body of a request containing information about comment
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse create(CommentDto commentDto, HttpStatus status) {
         LOGGER.info("Create new Comment");
         return post(
@@ -33,11 +60,30 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends PUT request to "/comments/{commentID}" endpoint,
+    * updates an existing comment by id
+    * then extracts response body as CommentDto class instance
+    *
+    * @param id id of a comment to update
+    * @param commentDto body of a request containing information about comment
+    * @return response body extracted into CommentDto class instance
+    */
     public CommentDto update(int id, CommentDto commentDto) {
         return update(commentDto, id, HttpStatus.OK)
             .extract().as(CommentDto.class);
     }
 
+    /**
+    * Sends PUT request to "/comments/{commentID}" endpoint,
+    * updates an existing comment by id
+    *
+    * @param commentDto body of a request containing updated information about
+    * comment
+    * @param id id of a comment to update
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse update(CommentDto commentDto, int id, HttpStatus status) {
         LOGGER.info("Update Comment by id [{}]", id);
         return put(
@@ -48,11 +94,28 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends GET request to "/comments/{commentID}" endpoint,
+    * returns a comment by id
+    * then extracts response body as CommentDto class instance
+    *
+    * @param id id of a comment to get
+    * @return response body extracted into CommentDto class instance
+    */
     public CommentDto getById(int id) {
         return getById(id, HttpStatus.OK)
             .extract().as(CommentDto.class);
     }
 
+
+    /**
+    * Sends GET request to "/comments/{commentID}" endpoint,
+    * returns a comment by id
+    *
+    * @param id id of a comment to get
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse getById(int id, HttpStatus status) {
         LOGGER.info("Get Comment by id [{}]", id);
         return get(
@@ -62,10 +125,24 @@ public class CommentEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends GET request to "/comments" endpoint,
+    * gets all existing comments
+    * then extracts response body as a list of CommentDto class instances
+    *
+    * @return response body extracted into list of CommentDto class instances
+    */
     public List<CommentDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(CommentDto[].class));
     }
 
+    /**
+    * Sends GET request to "/comments" endpoint,
+    * gets all existing comments
+    *
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Comments");
         ValidatableResponse response = get(this.specification, COMMENTS_END);
--- UserEndpoint.java
+++ UserEndpoint.java
@@ -9,21 +9,48 @@ import com.softserve.taf.models.enums.HttpStatus;
 import com.softserve.taf.models.placeholder.user.UserDto;
 import com.softserve.taf.services.common.AbstractWebEndpoint;
 
+/**
+* UserEndpoint is a class for sending REST requests to "/users" endpoint
+*
+* @author      Vasyl Boliuk
+* @version     %I%, %G%
+*/
 public class UserEndpoint extends AbstractWebEndpoint {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String USERS_END = "/users";
     private static final String USERS_RESOURCE_END = "/users/{userID}";
 
+    /**
+    * Creates an instance of UserEndpoint
+    *
+    * @param specification request specification for HTTP request
+    */
     public UserEndpoint(RequestSpecification specification) {
         super(specification);
     }
 
+    /**
+    * Sends POST request to "/users" endpoint,
+    * creates a new user
+    * then extracts response body as UserDto class instance
+    *
+    * @param userDto body of a request containing information about user
+    * @return response body extracted into UserDto class instance
+    */
     public UserDto create(UserDto userDto) {
         return create(userDto, HttpStatus.CREATED)
             .extract().as(UserDto.class);
     }
 
+    /**
+    * Sends POST request to "/users" endpoint,
+    * creates a new user
+    *
+    * @param userDto body of a request containing information about user
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse create(UserDto userDto, HttpStatus status) {
         LOGGER.info("Create new User");
         return post(
@@ -33,11 +60,29 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends PUT request to "/users/{userID}" endpoint,
+    * updates existing user by id
+    * then extracts response body as UserDto class instance
+    *
+    * @param id id of a user to update
+    * @param userDto body of a request containing information about user
+    * @return response body extracted into UserDto class instance
+    */
     public UserDto update(int id, UserDto userDto) {
         return update(userDto, id, HttpStatus.OK)
             .extract().as(UserDto.class);
     }
 
+    /**
+    * Sends PUT request to "/users/{userID}" endpoint,
+    * updates existing user by id
+    *
+    * @param id id of a user to update
+    * @param userDto body of a request containing information about user
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse update(UserDto userDto, int id, HttpStatus status) {
         LOGGER.info("Update User by id [{}]", id);
         return put(
@@ -48,11 +93,27 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends GET request to "/users/{userID}" endpoint,
+    * gets an existing user by id
+    * then extracts response body as UserDto class instance
+    *
+    * @param id id of a user to get
+    * @return response body extracted into UserDto class instance
+    */
     public UserDto getById(String id) {
         return getById(id, HttpStatus.OK)
             .extract().as(UserDto.class);
     }
 
+    /**
+    * Sends GET request to "/users/{userID}" endpoint,
+    * gets an existing user by id
+    *
+    * @param id id of a user to get
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse getById(String id, HttpStatus status) {
         LOGGER.info("Get User by id [{}]", id);
         return get(
@@ -62,10 +123,24 @@ public class UserEndpoint extends AbstractWebEndpoint {
             .statusCode(status.getCode());
     }
 
+    /**
+    * Sends GET request to "/users" endpoint,
+    * gets all existing users
+    * then extracts response body as a list of UserDto class instances
+    *
+    * @return response body extracted into a list of UserDto class instances
+    */
     public List<UserDto> getAll() {
         return List.of(getAll(HttpStatus.OK).extract().as(UserDto[].class));
     }
 
+    /**
+    * Sends GET request to "/users" endpoint,
+    * gets all existing users
+    *
+    * @param status expected response status
+    * @return REST Assured ValidatableResponse object
+    */
     public ValidatableResponse getAll(HttpStatus status) {
         LOGGER.info("Get all Users");
         ValidatableResponse response = get(this.specification, USERS_END);
